9757a60c0d227306339c88989c23a7ae71398958
Oliver <olivernie2626@gmail.com>
2025-03-22 23:51:53 -0500

    formulario nuevo

--- frontend/src/components/ContactForm.js
+++ frontend/src/components/ContactForm.js
@@ -19,21 +19,40 @@ const App = () => {
 
     return (
         <div className="App">
-            <div className="form-container"> 
-            <h1>Contacto</h1>
+            <div className="form-container">
+                <form onSubmit={handleSubmit(onSubmit)} className='contact-form'>
+                    <div class="form-header">
+                        <h1>Contáctanos</h1>
+                    </div>
 
-            <form onSubmit={handleSubmit(onSubmit)} className='form'>
-                    <label>Nombre:</label>
-                    <input type="text" {...register('name', { required: true })} placeholder='Oliver'/>
-                    
-                    <label>Email:</label>
-                    <input type="email" {...register('email', { required: true })} placeholder='tucorreo@example.com'/>
+                    <div className="form-group">
+                        <input type="text" {...register('name', { required: true })} placeholder='Oliver' />
+                        <label for="nombre">Nombre</label>
+                        <span className="focus-border"></span>
+                    </div>
 
-                    <label>Mensaje:</label>
-                    <textarea {...register('message', { required: true })} placeholder='Hola buenas tardes...'></textarea>
-                <button type="submit">Enviar</button>
-            </form>
-        </div>
+                    <div className="form-group">
+                        <input type="email" {...register('email', { required: true })} placeholder='tucorreo@example.com' />
+                        <label for="email">Email</label>
+                        <span className="focus-border"></span>
+                    </div>
+
+                    <div className="form-group">
+                        <textarea {...register('message', { required: true })} placeholder='Hola buenas tardes...'></textarea>
+                        <label for="mensaje">Mensaje</label>
+                        <span class="focus-border"></span>
+                    </div>
+
+                    <button class="submit-btn" type="submit">
+                        <span class="btn-text">Enviar</span>
+                        <span class="btn-icon">
+                            <svg viewBox="0 0 24 24" width="24" height="24">
+                                <path d="M2.01 21L23 12 2.01 3 2 10l15 2-15 2z"></path>
+                            </svg>
+                        </span>
+                    </button>
+                </form>
+            </div>
         </div>
     );
 };
